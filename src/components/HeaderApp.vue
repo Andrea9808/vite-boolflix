21d0f3c60d2f78830501066d583ab530004dd78c
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-01-10 13:08:32 +0100

    feat:header

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,41 @@
+<script>
+import SearchApp from './SearchApp.vue';
+
+export default {
+
+    name: "HeaderApp",
+
+    components: {
+        SearchApp,
+    }
+}
+</script>
+
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="intro">
+                <div class="logo">
+                    <h3>BollFlix</h3>
+                </div>
+
+                <div>
+                    <SearchApp />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .intro{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        min-height: 80px;
+
+        .logo{
+            color: red;
+        }
+    }
+</style>
---
+++ src/components/SearchApp.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "SearchApp"
+}
+</script>
+
+<template>
+    <input type="text" placeholder="Inserisci il film o la serie che vuoi vedere">
+    <button>INIZIA</button>
+</template>
+
+<style></style>
